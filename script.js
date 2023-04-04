51e9cf4d228931bea9900bbc64e857e7cdcd83c6
Jogi <stigmahotspot@gmail.com>
2023-04-04 16:29:27 +0700

    masih sampai langkah 5

---
+++ script.js
@@ -0,0 +1,69 @@
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number)=>{
+    number.addEventListener("click", (event)=> {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+});
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    if(currentNumber === '0'){
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) =>{
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    pervNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ''
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () =>{
+    calculate()
+    updateScreen(currentNumber)
+})
+
+const calculate = () => {
+    let result = ''
+    switch(calculationOperator){
+        case '+':
+            result = parseInt(prevNumber) + parseInt(currentNumber)
+            break
+        case '-':
+            result = prevNumber - currentNumber
+            break
+        case '*':
+            result = prevNumber * currentNumber
+            break
+        case '/':
+            result = prevNumber / currentNumber
+            break
+        default:
+            break
+    }
+    currentNumber = result
+    calculationOperator = ''
+}
